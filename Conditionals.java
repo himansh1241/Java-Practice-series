c6c7036c2e6c9b22ae291452973502920541ae6a
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-04-03 20:55:48 +0530

    Added else condition

--- Conditionals.java
+++ Conditionals.java
@@ -1,9 +1,17 @@
 public class Conditionals {
     public static void main(String[] args) {
 
-        int age = 100;
-        if (age > 18) {
-            System.out.println("You're eligible to vote");
+        // int age = 100;
+        // if (age > 18) {
+        //     System.out.println("You're eligible to vote");
+        // }
+
+        int score = 42;
+
+        if (score >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
         }
     }
 }
