db03d6d86b50d78f819d8be5a1902728e3b7e3d8
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-04-03 20:49:24 +0530

    Initial commit

---
+++ Conditionals.java
@@ -0,0 +1,9 @@
+public class Conditionals {
+    public static void main(String[] args) {
+
+        int age = 100;
+        if (age > 18) {
+            System.out.println("You're eligible to vote");
+        }
+    }
+}
