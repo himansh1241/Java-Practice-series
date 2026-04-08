e7b0a742432c36c587369acd71b30a35e6ed7eed
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-04-08 21:06:08 +0530

    Initial Commit

---
+++ CakeDiscount.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner hr = new Scanner(System.in);
+		int N = hr.nextInt();
+		int cost;
+		
+		if (N >= 5) {
+		    cost = N * 85;
+		} else {
+		    cost = N * 100;
+		}
+        
+        System.out.println(cost);
+        
+        hr.close();
+	}
+}
