c3ad5d7a3337b540f7ae4e8b455609346b633b29
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-05-31 13:08:08 +0530

    Initial Commit

---
+++ SmallestNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner hr = new Scanner(System.in);
+
+        System.out.print("Enter two nos: ");
+        int a = hr.nextInt();
+        int b = hr.nextInt();
+
+        if (a < b) {
+            System.out.println(a + " is smaller than " + b);
+        }
+        else {
+            System.out.println(b + " is smaller than " + a);
+        }
+
+    }
+}
