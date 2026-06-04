ea7a515899895481893c5f988bd1c18a0b3b8e32
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-04 23:28:38 +0530

    Initial Commit

---
+++ FunctionsTutorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FuctionsTutorial {
+
+    public static int printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        return 0;
+    }
+
+    public static void calculateSum(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println("Sum is : " + sum);
+    }
+    public static void main(String[] args) {
+        Scanner hr = new Scanner(System.in);
+        int a = hr.nextInt(); 
+        int b = hr.nextInt();
+        calculateSum(a, b);
+    }
+}
