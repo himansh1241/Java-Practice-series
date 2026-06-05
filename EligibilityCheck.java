c2297e39a4951a4df0f5663da421549860d98332
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-05 21:04:45 +0530

    Initial Commit

---
+++ EligibilityCheck.java
@@ -0,0 +1,16 @@
+// Take age input and print if he/she is eligible to vote or not
+
+import java.util.Scanner;
+
+public class EligibilityCheck {
+    public static void main(String[] args) {
+        Scanner hr = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = hr.nextInt();
+
+        String result = (age >= 18) ? "Eligible to vote" : "Not Eligible";
+        System.out.println(result);
+
+        hr.close();
+    }
+}
