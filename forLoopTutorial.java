e42adf3a69757339096c605bf5fff0f851d799e5
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-07 00:10:06 +0530

    Added new segment

--- forLoopTutorial.java
+++ forLoopTutorial.java
@@ -4,18 +4,26 @@ public class forLoopTutorial {
     public static void main(String[] args) {
         Scanner hr = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Value of i: " + i);
-        }
+        int i = 1;
 
-        for (int i = 1; i <=4; i++) {
-            System.out.println("Himanshu..");
-        }
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.println("Value of i: " + i);
+        // }
 
-        for (int i = 1; i <= 10; i += 2) {
-            System.out.println(i);
-        }
+        // for (int i = 1; i <=4; i++) {
+        //     System.out.println("Himanshu..");
+        // }
 
+        // for (int i = 2; i <= 20; i += 2) {
+        //     System.out.println(i);
+        // }
+
+        for (i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         hr.close();
     }
 }
