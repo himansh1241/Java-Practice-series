440fa695805b9b18674c86a53818df41a75ae9b0
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-06 23:53:58 +0530

    Added new statement

--- forLoopTutorial.java
+++ forLoopTutorial.java
@@ -12,6 +12,10 @@ public class forLoopTutorial {
             System.out.println("Himanshu..");
         }
 
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+
         hr.close();
     }
 }
