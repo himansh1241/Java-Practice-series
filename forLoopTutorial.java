5fadb771b8f4ec0904d29d66b0e5154819bb114f
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-06 23:45:14 +0530

    Initial Commit

---
+++ forLoopTutorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class forLoopTutorial {
+    public static void main(String[] args) {
+        Scanner hr = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Value of i: " + i);
+        }
+
+        hr.close();
+    }
+}
