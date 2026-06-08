c6e19e90717c532f2e716bb0486a1b16dcee0a41
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-08 23:38:21 +0530

    Initial Commit

---
+++ LoopsQ5.java
@@ -0,0 +1,23 @@
+// Print all prime numbers from 1 to 100
+
+import java.util.Scanner;
+
+public class LoopsQ5 {
+    public static void main(String[] args) {
+
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }    
+}
