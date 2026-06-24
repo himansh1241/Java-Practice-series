ebebeab9fe4a7e8066a53008c3c1294cdb71b0d2
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-24 21:00:51 +0530

    Modified condition

--- FunctionsOverloadingQ3.java
+++ FunctionsOverloadingQ3.java
@@ -17,8 +17,13 @@ public class FunctionsOverloadingQ3 {
     //     return isPrime;
 
     public static boolean isPrime(int n) {
+
+        if (n <= 1) {
+            return false;
+        }
+        
         for (int i = 2; i <= n-1; i++) {
-            if (n % 2 == 0){
+            if (n % i == 0){
                 return false;
             }
         }
